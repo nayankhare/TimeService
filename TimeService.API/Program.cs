3901ca49fefee863156f2f255bb89264083335b4
EJZ4868@hca.corpad.net <Nayan.Khare@Parallon.com>
2024-04-29 13:20:10 +0530

    Add project files.

---
+++ TimeService.API/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapGet("utctime", () => Results.Ok(DateTime.UtcNow));
+
+app.Run();
