ba8465125010b1ac6214a125a49fce2796954e8c
EJZ4868@hca.corpad.net <Nayan.Khare@Parallon.com>
2024-05-03 19:10:34 +0530

    Uncommented endpoint hello

--- TimeService.API/Program.cs
+++ TimeService.API/Program.cs
@@ -14,8 +14,8 @@ app.UseHttpsRedirection();
 
 app.MapGet("utctime", () => Results.Ok(DateTime.UtcNow));
 app.MapGet("time", () => Results.Ok(DateTime.Now));
-/*
+
 app.MapGet("hello", () => Results.Ok("Hello World!"));
-*/
+
 
 app.Run();
